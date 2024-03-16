8e572287055aaaa0b8b733aceecba70b4639b6eb
DougCruzz10 <douglascruztst11@gmail.com>
2024-03-16 18:38:57 -0400

    update: Regra_de_3_simples updated

--- regra_de_3/regra_de_3_simples.py
+++ regra_de_3/regra_de_3_simples.py
@@ -1,4 +1,5 @@
 from modules.input_letters import input_letras
+from modules.get_input import valida_numero
 
 def regra_simples():
     greatness = []
@@ -8,4 +9,33 @@ def regra_simples():
     great_one = input_letras('Digite a primeira grandeza: ')
     great_two = input_letras('Digite a segunda grandeza: ')
 
+    pop_up = ['S', 'N']
+    confirm = input_letras(f'{great_one}, {great_two}: Deseja confirmar: [S/N]> ').upper()
+    
+    while True:
+        if confirm not in pop_up:
+            confirm = input_letras(f'{great_one}, {great_two}: Deseja confirmar: [S/N]> ').upper()
+        else:
+            break
+    
+    
+    while True:
+        if confirm == 'S':
+            greatness.append(great_one,great_two)
+            break
+        else:
+            great_one = input_letras('Digite a primeira grandeza: ')
+            great_two = input_letras('Digite a segunda grandeza: ')
+            confirm = input_letras(f'{great_one}, {great_two}: Deseja confirmar: [S/N]> ').upper()
+
+            
+
+                
+    
+    
+    
 regra_simples()
+
+
+
+        
