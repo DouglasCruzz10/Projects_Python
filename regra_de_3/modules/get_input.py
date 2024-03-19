9fc415a6ce61544ecf439e7e979916b06518fd27
João Gabriel Gomes <j.gabriel.gomes2976@gmail.com>
2024-03-18 22:05:32 -0400

    Update: Funcional Code of Regra de 3 Composta
    Versão Funcional do regra de 3 composta implementada, prevenções de certos erros ainda não inseridas

--- regra_de_3/modules/get_input.py
+++ regra_de_3/modules/get_input.py
@@ -1,6 +1,6 @@
 def valida_numero(string):
     # Remove espaços em branco e remove eventuais .(pontos) da string e depois
-    # verifica se possui apenas digitos númericos, se não tiver digitos númericos retorna falso e a função encerra
+    # verifica se possui apenas digitos númericos, se não tiver apenas digitos númericos retorna falso e a função encerra
     if not ((string.replace(' ', '')).replace('.', '')).isdigit(): 
         return False
     try:
@@ -9,9 +9,9 @@ def valida_numero(string):
     except ValueError: # se algum erro acontecer ao tentar converter para float acontecer retorna Falso, significando que 
         return False   # a string não era válida
 
-# Exemplo de uso:
+# #Exemplo de uso:
 # entrada = input("Digite um valor: ")
-# if valida_numero_simples(entrada):
+# if valida_numero(entrada):
 #     print("A entrada é um número válido.")
 # else:
 #     print("Digite apenas números!")
--- regra_de_3/regra_de_3_composta.py
+++ regra_de_3/regra_de_3_composta.py
@@ -1,4 +1,6 @@
 from modules.get_input import valida_numero
+from modules.has_x import has_x
+from modules.multiplication import mult_list
 
 def regra_de_3_composta():
     names_greatness_list = []
@@ -30,19 +32,115 @@ def regra_de_3_composta():
         else:
             greatness_qtd_int = int(greatness_qtd_str)
             break
-
+    
+    print('Quando for determinar a incognita use "X" !')
+    X_store = False # variável irá me dizer se o valor X já foi designado
     # um loop for que se repetirá de acordo com a quantidade de grandezas escolhidas
     for i in range(0, greatness_qtd_int):
         # faz o usuário digitar o nome das grandezas de acordo com o número de grandezas que ele escolheu
         print(f'Digite o nome da {i+1}º grandeza:', end=' ')
         names_greatness_list.append(input())
 
-        # insere o valor inicial da grandeza na lista
-        print(f'Digite o valor inicial da grandeza {names_greatness_list[i]}:', end=' ')
-        startvalue_greatness_list.append(input())
+        # insere o valor inicial da grandeza numa variável
+        startvalue_greatness = input(f'Digite o valor inicial da grandeza {names_greatness_list[i]}: ')
 
-        # insere o valor final da grandeza na lista
-        print(f'Digite o valor final da grandeza {names_greatness_list[i]}:', end=' ')
-        finalvalue_greatness_list.append(input())
+        # irá verificar se o valor da variável é x, se minúsculo converte pra maiúscula, depois adiciona na lista.
+        if startvalue_greatness == 'x':
+            startvalue_greatness = startvalue_greatness.upper()
+        if startvalue_greatness == 'X':
+            startvalue_greatness_list.append(startvalue_greatness)
+        
+        # se o valor conter qualquer letra que não seja o "X" faz com que o usuário caia num loop até que digite corretamente
+        # um valor numerico.
+        elif not valida_numero(startvalue_greatness):
+            while True:
+                startvalue_greatness = input('ERRO! Digite apenas números ou "X"!: ')
+                if startvalue_greatness == 'x':
+                    startvalue_greatness = startvalue_greatness.upper()
+                    break
+                if valida_numero(startvalue_greatness) and startvalue_greatness != 'X':
+                    break
+            startvalue_greatness_list.append(startvalue_greatness)
+        else:
+            startvalue_greatness_list.append(startvalue_greatness)
+        
+        # insere o valor final da grandeza numa variável
+        finalvalue_greatness = input(f'Digite o valor final da grandeza {names_greatness_list[i]}: ')
 
-regra_de_3_composta()
+        # irá verificar se o valor da variável é x, se minúsculo converte pra maiúscula, depois adiciona na lista.
+        if finalvalue_greatness == 'x':
+            finalvalue_greatness = finalvalue_greatness.upper()
+        if finalvalue_greatness == 'X':
+            finalvalue_greatness_list.append(finalvalue_greatness)
+        
+        # se o valor conter qualquer letra que não seja o "X" faz com que o usuário caia num loop até que digite corretamente
+        # um valor numerico.    
+        elif not valida_numero(finalvalue_greatness):
+            while True:
+                finalvalue_greatness = input('ERRO! Digite apenas números ou "X"!: ')
+                if finalvalue_greatness == 'x':
+                    finalvalue_greatness = finalvalue_greatness.upper()
+                    break
+                if valida_numero(finalvalue_greatness) and finalvalue_greatness != 'X':
+                    break
+            finalvalue_greatness_list.append(finalvalue_greatness)
+        else:
+            finalvalue_greatness_list.append(finalvalue_greatness)
+        
+    # vai pedir do usuário para identificar qual grandeza é o produto, e vai retornar o indice de onde 
+    # está o produto na lista de grandezas
+    print('Produto é a grandeza na qual as outras grandezas operam em prol. ')
+    product = input(f'Quais dessas grandezas {names_greatness_list} é o produto? [DIGITE DA MESMA FORMA] :')
+
+    # retorna -1 para a variavel indice do produto caso o valor de produto não esteja na lista de grandezas
+    try:
+        index_of_product = names_greatness_list.index(product)
+    except ValueError:
+            index_of_product = -1
+        
+    # se o indice do produto for -1 bota o usuário num loop até que ele digite corretamente o produto
+    if index_of_product == -1:
+        while True:
+            product = input('DIGITE A GRANDEZA CORRETAMENTE: ')
+            index_of_product = names_greatness_list.index(product)
+            if not index_of_product == -1:
+                break
+
+    # vai inverter o valor inicial e final dentro da lsta da grandeza que for o produto
+    store_of_startvalue = startvalue_greatness_list[index_of_product]
+    startvalue_greatness_list[index_of_product] = finalvalue_greatness_list[index_of_product]
+    finalvalue_greatness_list[index_of_product] = store_of_startvalue
+
+    # irá armazenar em qual lista (de iniciais e finais) que está o 'X'
+    line_of_x = 'start list' if has_x(startvalue_greatness_list) else 'final list'
+
+    # irá armazenar o indice de X
+    index_of_x = startvalue_greatness_list.index('X') if has_x(startvalue_greatness_list) else finalvalue_greatness_list.index('X')
+
+    # lógica do cálculo:
+    # se o x estiver na lista de valores iniciais, vai remover o elemento 'X', multiplicar toda a lista inicial e final
+    # e por fim vai dividir final por inicial
+    if line_of_x == 'start list':
+        startvalue_greatness_list.remove('X')
+
+        # coverte as listas para float
+        startvalue_greatness_list = [float(value.replace(',', '.')) for value in startvalue_greatness_list]
+        finalvalue_greatness_list = [float(value.replace(',', '.')) for value in finalvalue_greatness_list]
+
+        multiplication_startvalues = mult_list(startvalue_greatness_list)
+        multiplication_finalvalues = mult_list(finalvalue_greatness_list)
+        result = multiplication_finalvalues / multiplication_startvalues
+    # se o x estiver na lista de valores finais, vai remover o elemento 'X', multiplicar toda lista final e inicial
+    # e por fim vai dividir inicial por final  
+    else:
+        finalvalue_greatness_list.remove('X')
+
+        # converte as listas para float
+        startvalue_greatness_list = [float(value.replace(',', '.')) for value in startvalue_greatness_list]
+        finalvalue_greatness_list = [float(value.replace(',', '.')) for value in finalvalue_greatness_list]
+
+        multiplication_finalvalues = mult_list(finalvalue_greatness_list)
+        multiplication_startvalues = mult_list(startvalue_greatness_list)
+        result = multiplication_startvalues / multiplication_finalvalues
+
+    print(f'O valor de "X" é: {result:.2f} {names_greatness_list[index_of_x]}')
